2f4e031984e979d88971aed54d33d2bdf6628c40
Your Name <you@example.com>
2022-08-10 20:32:17 +0530

    C3

--- Vpc1.tf
+++ Vpc1.tf
@@ -1,18 +1,27 @@
 variable "v_vpc_cidr"{
 }
+/* Tags Added by Harsha */
 resource "aws_vpc" "vpc1"{
 	cidr_block=var.v_vpc_cidr
+	tags={
+		Name="VPC1"
+	}
 }
 resource "aws_subnet" "sn1"{
 	vpc_id=aws_vpc.vpc1.id
 	cidr_block="10.0.0.0/24"
 	avilability_zone="ap-south-1a"
+	tags={
+		Name="SN1-VPC1"
+	}
 	
 }
 resource "aws_subnet" "sn2"{
 	vpc_id=aws_vpc.vpc1.id
 	cidr_block="10.0.1.0/24"
 	avilability_zone="ap-south-1a"
-	
+	tags={
+		Name="SN2-VPC1"
+	}
 }
 	
