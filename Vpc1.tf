63189d627ab1c4e616bc88fa57a1c23537b8cdb6
Your Name <you@example.com>
2022-08-10 20:24:58 +0530

    c1

---
+++ Vpc1.tf
@@ -0,0 +1,12 @@
+variable "v_vpc_cidr"{
+}
+resource "aws_vpc" "vpc1"{
+	cidr_block=var.v_vpc_cidr
+}
+resource "aws_subnet" "sn1"{
+	vpc_id=aws_vpc.vpc1.id
+	cidr_block="10.0.0.0/24"
+	avilability_zone="ap-south-1a"
+	
+}
+	
